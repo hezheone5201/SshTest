3b81579b4afd8a08fff3fe9ac1c8aa7bb35cf6b9
hezheone5201 <hezheone5201@163.com>
2018-01-26 17:21:46 +0800

    Update MainActivity.java
    测试冲突

--- mydemo/app/src/main/java/com/example/x/myapplication/MainActivity.java
+++ mydemo/app/src/main/java/com/example/x/myapplication/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //test conflict
         setContentView(R.layout.activity_main);
     }
 }
