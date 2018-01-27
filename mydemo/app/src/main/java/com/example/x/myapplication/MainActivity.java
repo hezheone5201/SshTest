fe61e567a38eb5a960dfd3759aa7daa7f99afd27
hezheone5201 <hezheone5201@163.com>
2018-01-27 09:33:14 +0800

    ss
    sssss

--- mydemo/app/src/main/java/com/example/x/myapplication/MainActivity.java
+++ mydemo/app/src/main/java/com/example/x/myapplication/MainActivity.java
@@ -8,10 +8,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-         //
+         //是是是
         //
         //
-        //
+        //试试
+        //sss
+        //ssss
+        //sss
+        //ssss
         setContentView(R.layout.activity_main);
     }
 }
